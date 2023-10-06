ae84249e1e67b6ed2f28b84e9a883b45cb8b6691
Michie <contact@michiewillman.com>
2023-10-05 21:15:23 -0700

    Add default symptoms and mental state answers

---
+++ project-react/server/seeders/defaultSymptoms.js
@@ -0,0 +1,31 @@
+const defaultSymptoms = [
+  "Difficulty swallowing",
+  "Difficulty chewing",
+  "Difficulty talking",
+  "Shortness of breath",
+  "Droopy eyelid(s)",
+  "Eye/eyelid movement",
+  "Blurred vision",
+  "Double vision",
+  "Muscle weakness",
+  "General fatigue",
+  "Facial expressions",
+];
+
+const mentalState = [
+  "Happy",
+  "Hopeful",
+  "Fine",
+  "Sad",
+  "Discouraged",
+  "Depressed",
+  "Suicidal thoughts",
+];
+
+// DANGER alert with difficuly swallowing, chewing or shortness of breath responses
+
+// GIVE Encouragement with depressed response
+
+// ALERT with suicidal thoughts response
+
+const symptomLevel = ["None", "Slight", "Fair", "Bad", "Extreme"];
