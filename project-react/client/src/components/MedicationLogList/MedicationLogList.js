2f45bcdc653b3f93c2bb2ee6ce8e76b906db6ca1
Michie <contact@michiewillman.com>
2023-10-13 01:23:29 -0700

    med log list seems OK

--- project-react/client/src/components/MedicationLogList/MedicationLogList.js
+++ project-react/client/src/components/MedicationLogList/MedicationLogList.js
@@ -2,61 +2,50 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_MEDICATION_LOGS } from "../../utils/queries";
 import MedLogCard from "../MedLogCard/MedLogCard";
+// import spinner from "../../assets/Spin-1s-200px.gif";
 
-const MedicationLogList = ({ userId }) => {
+const MedicationLogList = ({ datetime }) => {
   // Get user's medications property (as array)
   const { loading, data } = useQuery(QUERY_MEDICATION_LOGS, {
-    variables: { _id: userId },
+    variables: { datetime },
   });
   const logs = data?.user || {};
-  console.log(logs);
+  console.log("query of logs", logs);
 
-  // if empty medications array, exit func
-  // Otherwise proceed...
-
-  // Handle "Take Medication" button click
-  // const handleTakeMedication = (event) => {
-  //   // Set medication as the buton's target value (name of medicine)
-  //   // Not sure if below is correct
-  //   const medication = event.target.value;
-  // };
-  // -- add new medication to array
-  // -- Update db of patient's medications
-  // -- Update state of patient's medications
-  // const deleteMedLog = () => {
-
-  if (loading) {
-    <p>Loading...</p>;
+  if (!logs.length) {
+    return <h3>You haven't logged anything today.</h3>;
   }
+
+  // TODO: Update/add new medication to patient's medications array
+
   return (
-    <div>
-      <h2>Medication Logs</h2>
-      <div className="list-group">
-        {/* {logs.map((log) => (
-          <MedLogCard key={log._id} />
-        ))} */}
-      </div>
+    <div className="my-2">
+      <h2>Medication</h2>
+      <p>{logs}</p>
+      {logs.length ? (
+        <div className="flex-row">
+          {logs.map((log) => (
+            <MedLogCard
+              key={log._id}
+              name={log.medicationName}
+              dosage={log.dosage}
+              time={log.datetime}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any medications yet.</h3>
+      )}
+      {/* <button onClick={}></button> */}
+      {/* Button "Take Medication" renders the AddMedLogForm modal / component */}
+      {/* {loading ? <img src={spinner} alt="loading" /> : null} */}
     </div>
   );
 };
 
 export default MedicationLogList;
 
-//  use flatlist for logs?
-//  Animated.sequence(log cards)
-
-// hidden input value = the medication log's id (in order to pass that in to remove medication by this id)
-
-// Render all logged medication entries for current date
-// {medication} taken at {time}
-
-// Remove medication log button --> deleteMedLog
-
-// Medication Cards with...
-// Medication Name
-// Dosage
-// Button ---> Take Medication
-
+// Future Development code: REACT NATIVE CONFIRM
 //   Alert.alert("Confirm delete", "Are you sure you want to delete this log?", [
 //     {
 //       text: "Cancel",
