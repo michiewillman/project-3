7dc90fbfe56dec007a6f95e17c6cdbca1d980ab6
Michie <contact@michiewillman.com>
2023-10-16 15:53:11 -0700

    Add new medication is working

---
+++ project-react/client/src/components/NewMedForm/NewMedForm.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER_MEDICATION } from "../../utils/mutations";
+import { PrimaryButton, SecondaryButton } from "../Button/Button";
+import Auth from "../../utils/auth";
+
+const NewMedForm = (props) => {
+  const [formState, setFormState] = useState("");
+
+  const [addUserMedication, { error }] = useMutation(ADD_USER_MEDICATION);
+
+  // Update state change with input change
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleAddMed = async (event) => {
+    event.preventDefault();
+
+    // Get the userId for our mutation
+    // const user = Auth.getUser();
+
+    // Add medication to the User's medications property (array)
+    try {
+      const data = await addUserMedication({
+        variables: formState,
+      });
+
+      // Clear the form by resetting the state
+      setFormState("");
+
+      // Close modal
+      props.toggleMedForm();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="overlay">
+      <div className="logModal">
+        <div className="relative w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+              <h3>Add Medication</h3>
+            </div>
+            <div className="p-6 space-y-6">
+              <form onSubmit={handleAddMed}>
+                <div className="col-12 col-lg-9">
+                  <label htmlFor="medication">Medication Name</label>
+                  <input
+                    className="formInput"
+                    name="medication"
+                    type="text"
+                    placeholder="enter new medication name"
+                    value={formState.medication}
+                    onChange={(event) => handleInputChange(event)}
+                  />
+                </div>
+                <SecondaryButton
+                  text="Cancel"
+                  action={props.toggleModal}
+                  type="button"
+                />
+                <PrimaryButton text="Submit" type="Submit" />
+                {error && (
+                  <div className="col-12 my-3 bg-danger text-white p-3">
+                    {error.message}
+                  </div>
+                )}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewMedForm;
--- project-react/client/src/components/UserMeds/UserMeds.js
+++ project-react/client/src/components/UserMeds/UserMeds.js
@@ -5,16 +5,25 @@ import firstToUppercase from "../../utils/firstToUppercase";
 import Grid from "@mui/material/Grid"; // Grid version 1
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import "./UserMeds.css";
+import NewMedForm from "../NewMedForm/NewMedForm";
 
 const UserMeds = (props) => {
   const userMeds = props.medications;
 
-  const [isShown, setIsShown] = useState(false); // Modal for MedLogForm showing or not
-  const [selectedMedication, setSelectedMedication] = useState(""); // Selected medication to render in the form
+  // State control for New Med Form
+  const [medFormShown, setMedForm] = useState(false);
+  // State control for New Medication Log Form
+  const [isShown, setIsShown] = useState(false);
+  // Selected medication to render in the form
+  const [selectedMedication, setSelectedMedication] = useState("");
 
   const toggleModal = (medName) => {
     setSelectedMedication(medName); // Set the selected medication before toggling the modal
     setIsShown(!isShown);
+  };
+
+  const toggleMedForm = () => {
+    setMedForm(!medFormShown);
   };
 
   return (
@@ -38,12 +47,13 @@ const UserMeds = (props) => {
               <AddBoxIcon
                 fontSize="large"
                 className="addIcon"
-                onClick={() => toggleModal("")} // Clear selectedMedication for the "Add Med" button
+                onClick={() => toggleMedForm("")} // Clear selectedMedication for the "Add Med" button
               />
             </Grid>
           </Grid>
         </div>
       </Grid>
+      {/* Modal for creating log when User takes medication */}
       {isShown && (
         <MedLogForm
           // Pass the selected medication as a prop to MedLogForm
@@ -51,6 +61,14 @@ const UserMeds = (props) => {
           shown={isShown}
           setIsShown={setIsShown}
           toggleModal={toggleModal}
+        />
+      )}
+      {/* Modal for adding a medication to User property */}
+      {medFormShown && (
+        <NewMedForm
+          medFormShown={medFormShown}
+          setMedForm={setMedForm}
+          toggleMedForm={toggleMedForm}
         />
       )}
     </>
